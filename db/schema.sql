7d8283a66c18eec9894cb08de91e795528dbae52
unknown <bradwayne74@gmail.com>
2018-02-08 12:53:54 -0500

    added jaws_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 ### Schema
--- CREATE DATABASE burgers_db;
--- USE burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 
 CREATE TABLE burgers
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 -- USE burgers_db;
--- -- SELECT * FROM burgers;
--- 
+SELECT * FROM burgers;
 
-INSERT INTO burgers (burger_name) VALUES ('bacon cheese burger');
-INSERT INTO burgers (burger_name) VALUES ('chili chedder burger');
-INSERT INTO burgers (burger_name) VALUES ('chipotle turkey burger');
-INSERT INTO burgers (burger_name) VALUES ('guinness burger');
+
+-- INSERT INTO burgers (burger_name) VALUES ('bacon cheese burger');
+-- INSERT INTO burgers (burger_name) VALUES ('chili chedder burger');
+-- INSERT INTO burgers (burger_name) VALUES ('chipotle turkey burger');
+-- INSERT INTO burgers (burger_name) VALUES ('guinness burger');
